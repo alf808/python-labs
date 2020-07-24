6d4ba32f835a21493c7bfa88c3843db3424b389b
alf808 <alf@akademe.net>
2020-07-24 12:41:38 -1000

    completed labs 03

--- 03_more_datatypes/2_lists/03_09_flatten.py
+++ 03_more_datatypes/2_lists/03_09_flatten.py
@@ -7,3 +7,10 @@ flattened_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 '''
 
 starting_list = [[1, 2, 3, 4], [5, 6], [7, 8, 9]]
+flattened_list = []
+
+for item_list in starting_list:
+    for item in item_list:
+        flattened_list.append(item)
+
+print(flattened_list)

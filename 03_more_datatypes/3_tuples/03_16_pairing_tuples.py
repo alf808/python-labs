ccd0dc3051a782796449117a2716b8a76c312b2e
alf808 <alf@akademe.net>
2020-07-24 17:03:41 -1000

    complete 03 tuple labs

--- 03_more_datatypes/3_tuples/03_16_pairing_tuples.py
+++ 03_more_datatypes/3_tuples/03_16_pairing_tuples.py
@@ -11,3 +11,14 @@ Note: This lab might be challenging! Make sure to discuss it with your mentor
 or chat about it on our forum.
 
 '''
+result_list = []
+temp = [5, 9, 1, 4, 7, 6, 3]
+temp.sort()
+if len(temp) % 2 != 0:
+    temp.append(0)
+
+for x in range(len(temp)//2):
+    i, j = temp.pop(0), temp.pop(0)
+    result_list.append((i,j))
+
+print(result_list)

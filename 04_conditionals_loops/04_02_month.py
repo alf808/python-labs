eb1b9af276c036160bcfdf11e29500f42c72b38a
alf808 <alf@akademe.net>
2020-07-24 22:34:23 -1000

    finished prime problem

--- 04_conditionals_loops/04_02_month.py
+++ 04_conditionals_loops/04_02_month.py
@@ -4,3 +4,10 @@ Take in a number from the user and print "January", "February", ...
 or other respectively. Use a "nested-if" statement.
 
 '''
+months = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"]
+print("This application will print the month that corresponds to a number.")
+user_input = int(input("Please enter a number between 1 and 12: "))
+if user_input in range(1,13):
+    print(months[user_input-1])
+else:
+    print("Other")
--- 04_conditionals_loops/04_04_prime.py
+++ 04_conditionals_loops/04_04_prime.py
@@ -2,3 +2,11 @@
 Print out every prime number between 1 and 100.
 
 '''
+for num in range(2, 101):
+    num_is_prime = True # assume num is prime
+    for i in range(2, num):
+        if num % i == 0:
+            num_is_prime = False
+            break # out of this for loop
+    if num_is_prime:
+        print(num)

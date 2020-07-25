11cf5def91e89dd04f2946608995ed97c2d5fe4a
alf808 <alf@akademe.net>
2020-07-24 16:23:33 -1000

    finished third tuple lab

--- 03_more_datatypes/3_tuples/03_14_list_of_tuples.py
+++ 03_more_datatypes/3_tuples/03_14_list_of_tuples.py
@@ -6,3 +6,14 @@ input = "hello world"
 result_list = [('h', 'e', 'l', 'l', 'o'), ('w', 'o', 'r', 'l', 'd')]
 
 '''
+input = "hello world"
+input_list = input.split()
+result_list = []
+
+for word in input_list:
+    temp_list = []
+    for character in word:
+        temp_list.append(character)
+    temp_tuple = tuple(temp_list)
+    result_list.append(temp_tuple)
+print(result_list)

fd32d9f2e8ba74212563a9ed1104836e2c78d66d
alf808 <alf@akademe.net>
2020-07-24 17:32:24 -1000

    finished 03_19_common

--- 03_more_datatypes/4_dictionaries/03_19_common.py
+++ 03_more_datatypes/4_dictionaries/03_19_common.py
@@ -11,3 +11,17 @@ dict_2 = {"a": 2, "c": 4 , "d": 2}
 result = {"a": 3, "b": 2, "c": 7 , "d": 2}
 
 '''
+dict_1 = {"a": 1, "b": 2, "c": 3}
+dict_2 = {"a": 2, "c": 4 , "d": 2}
+result = {}
+
+for k, v in dict_1.items():
+    result[k] = v
+    if dict_2.get(k):
+        result[k] += dict_2.get(k)
+
+for k, v in dict_2.items():
+    if not result.get(k):
+        result[k] = dict_2.get(k)
+        
+print(result)

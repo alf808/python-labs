e4dc06e2b82fa3316238c4918032a1bab943d4c5
alf808 <alf@akademe.net>
2020-07-24 22:50:23 -1000

    finished while and search 04 labs

--- 04_conditionals_loops/04_06_while.py
+++ 04_conditionals_loops/04_06_while.py
@@ -2,3 +2,8 @@
 Use a "while" loop to print out every fifth number counting from 1 to 1000.
 
 '''
+count = 1
+while count <= 1000:
+    if count % 5 == 0:
+        print(count)
+    count += 1
--- 04_conditionals_loops/04_07_search.py
+++ 04_conditionals_loops/04_07_search.py
@@ -4,3 +4,11 @@ Receive a number between 0 and 1,000,000,000 from the user.
 Use while loop to find the number - when the number is found exit the loop and print the number to the console.
 
 '''
+count = 0
+print("This application will print your number when it is found.")
+user_input = int(input("Please enter a number between 0 and 1000000000: "))
+while count <= user_input:
+    if count == user_input:
+        print(count)
+        break
+    count += 1

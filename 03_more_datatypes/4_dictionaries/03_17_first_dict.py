6b5df18f4cb60f136ddb533258b120b9c6ef82a2
alf808 <alf@akademe.net>
2020-07-24 17:08:02 -1000

    started dictionaries labs

--- 03_more_datatypes/4_dictionaries/03_17_first_dict.py
+++ 03_more_datatypes/4_dictionaries/03_17_first_dict.py
@@ -4,3 +4,7 @@ Write a script that creates a dictionary of keys, n and values n*n for numbers 1
 result = {1: 1, 2: 4, 3: 9, ...and so on}
 
 '''
+result = {}
+for x in range(1,11):
+    result[x] = x**2
+print(result)

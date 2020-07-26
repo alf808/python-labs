cb7f954edce18a77284f9c84de4e2efedc1a6030
alf808 <alf@akademe.net>
2020-07-25 15:53:33 -1000

    finished stats function

--- 06_functions/06_02_stats.py
+++ 06_functions/06_02_stats.py
@@ -6,8 +6,11 @@ Print these values to the console when calling the function.
 
 example_list = [1, 2, 3, 4, 5, 6, 7]
 
-def stats():
+def stats(lst):
   # define the function here
-  pass
+  total = sum(lst)
+  average = total / len(example_list)
+  return max(lst), min(lst), average, total
 
 # call the function below here
+print(stats(example_list))

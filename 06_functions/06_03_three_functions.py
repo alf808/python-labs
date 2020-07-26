240442487b3d5bef2b94571821308f7fd1be813f
alf808 <alf@akademe.net>
2020-07-25 16:14:23 -1000

    completed 06_functions labs

--- 06_functions/06_03_three_functions.py
+++ 06_functions/06_03_three_functions.py
@@ -1,5 +1,24 @@
 '''
 Write a program with 3 functions. Each function must call
 at least one other function and use the return value to do something.
-
+circle
+area = pi * radius**2
+circum = 2 * pi * radius
+radius = diameter / 2
 '''
+PI = 3.14159
+def radius_circle(diameter):
+    return diameter / 2
+
+def area(diameter):
+    radius = radius_circle(diameter)
+    return PI * radius**2
+    
+def circumference(diameter):
+    radius = radius_circle(diameter)
+    return 2 * PI * radius
+
+def sphere_surface_area(diameter):
+    return 4 * area(diameter)
+    
+    

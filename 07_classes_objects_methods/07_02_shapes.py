263f0277dfdcce1717ba9fc924176f990eda96be
alf808 <alf@akademe.net>
2020-07-26 21:47:56 -1000

    finished shape classes

--- 07_classes_objects_methods/07_02_shapes.py
+++ 07_classes_objects_methods/07_02_shapes.py
@@ -9,8 +9,36 @@ perimeter (of the rectangle) and circumference of the circle.
 '''
 class Rectangle:
     """class that models a rectangle"""
-    def __init__(self, height, width)
+
+    def __init__(self, length, width):
+        """constructor with length and width"""
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+    def perimeter(self):
+        return self.length * 2 + self.width * 2
+
 
 class Circle:
     """class that models a circle"""
-    def __init__(self, radius)
+
+    PI = 3.14159
+
+    def __init__(self, radius):
+        """constructor with radius"""
+        self.radius = radius
+
+    def area(self):
+        return self.PI * self.radius**2
+
+    def circumference(self):
+        return 2 * self.PI * self.radius
+
+
+circ = Circle(3)
+squ = Rectangle(2,2)
+print(circ.circumference())
+print(squ.area())

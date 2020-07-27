8ea8eaf0f1c5ebb3ca567b1260d5e8a4d4b01bd3
alf808 <alf@akademe.net>
2020-07-27 09:30:03 -1000

    completed 08 file_io labs

--- 08_file_io/08_02_writing.py
+++ 08_file_io/08_02_writing.py
@@ -2,3 +2,15 @@
 Write a script that reads in the contents of words.txt and writes the contents in reverse
 to a new file words_reverse.txt.
 '''
+# assumption: single words per line
+input_list =[]
+word_list =[]
+with open("words.txt", "r") as fin:
+    input_list = fin.readlines()
+    # take out newline characters
+    word_list = [word.rstrip() for word in input_list]
+    word_list.reverse()
+
+with open("words_reverse.txt", "w") as fout:
+    # write to file with newline characters
+    fout.write("\n".join(word_list))

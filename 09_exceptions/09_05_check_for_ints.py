5f34bc0a32c79a2aadaad6b408fdbe54ef131884
alf808 <alf@akademe.net>
2020-07-28 12:43:14 -1000

    finished check_for_ints lab

--- 09_exceptions/09_05_check_for_ints.py
+++ 09_exceptions/09_05_check_for_ints.py
@@ -6,3 +6,12 @@ the input was an integer or not.
 The script should keep prompting the user until they enter an integer.
 
 '''
+while True:
+    try:
+        user_input = int(input("Please enter an integer: "))
+    except ValueError:
+        print("It must be an integer input.")
+        continue
+    else:
+        print("you have entered an integer. bye")
+        break

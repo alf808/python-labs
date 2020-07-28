9f84dc4c1773e6a049cbc999f997bcf475f64039
alf808 <alf@akademe.net>
2020-07-27 22:40:47 -1000

    finished exception handling with else block

--- 09_exceptions/09_03_else.py
+++ 09_exceptions/09_03_else.py
@@ -2,3 +2,14 @@
 Write a script that demonstrates a try/except/else.
 
 '''
+while True:
+    try:
+        user_input1 = int(input("Please enter first number: "))
+        user_input2 = int(input("Please enter second number: "))
+        print(user_input1 / user_input2)
+    except ZeroDivisionError as zde:
+        print(f"\nwhy do this: {zde}\n")
+    except ValueError:
+        print("\nYou were told to enter numbers!\n")
+    else:
+        print("\nYou followed instructions. Let\'s do this again. OK!")

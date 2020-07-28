1834327870d5e3c93d968679f312ae28244c7661
alf808 <alf@akademe.net>
2020-07-28 13:33:08 -1000

    finished check_files lab

--- 09_exceptions/09_06_check_files.py
+++ 09_exceptions/09_06_check_files.py
@@ -6,5 +6,23 @@ with specific except statements, and continue to do the calculation
 only if neither of them applies.
 
 '''
+import os
 
+script_path = os.path.dirname(os.path.realpath(__file__))
+books_path = os.path.join(script_path, "books")
 file_name = 'integers.txt'
+file = f"{script_path}/{file_name}"
+
+with open(file, "r") as fin:
+    temp_list = fin.readlines()
+    for num in temp_list:
+        try:
+            x = int(num) / 7
+        except TypeError:
+            print("type error")
+        except ValueError:
+            print("not an integer")
+        except IOError as ioe:
+            print(f"got this error message: {ioe}")
+        else:
+            print(x)

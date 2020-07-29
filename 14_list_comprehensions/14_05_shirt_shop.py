34598b65a96721a4d5408ecbd6af331aaff73943
alf808 <alf@akademe.net>
2020-07-29 08:29:38 -1000

    completed list comprehensions labs

--- 14_list_comprehensions/14_05_shirt_shop.py
+++ 14_list_comprehensions/14_05_shirt_shop.py
@@ -5,6 +5,8 @@ of the given lists.
 Then open up your online shop ;)
 
 '''
-
+# code based on https://stackoverflow.com/questions/52192855/cartesian-product-of-two-lists-in-python/52192888
 colors = ["neon orange", "spring green"]
 sizes = ["S", "M", "L"]
+products = [f"{color} in {size}" for color in colors for size in sizes]
+print(products)

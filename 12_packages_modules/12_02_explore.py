c12b6f3cc8f6e653ade062d9fa17ad38db86dd01
alf808 <alf@akademe.net>
2020-07-28 22:19:52 -1000

    explored some python packages

--- 12_packages_modules/12_02_explore.py
+++ 12_packages_modules/12_02_explore.py
@@ -2,4 +2,17 @@
 Do some research on other popular python packages and what the are used for. Feel free to import them
 and play around a little.
 
+opencv-python
+OpenCV is an ideal image processing package that allows you to both read and write images at the same time. 
+Computer Vision allows you to rebuild, interrupt, and comprehend a 3D environment from its respective 2D environment.
+This package allows you to diagnose special objects in any videos or images. Objects such as faces, eyes, trees, etc.
+
+nltk
+The text processing libraries of NLTK allow classification, tagging, tokenization, stemming, parsing, and semantic reasoning as well. 
+NLTK contains a graphical illustration of data science. It also comes with a handbook for guiding through the principles of language processing for NLTK. 
+It is open source and contains over fifty corpora and lexical resources such as open multilingual wordnet, question classification, SentiWordNet, SEMCOR, Stopwords Corpus, and many more. 
+NLTK also features structure types, structure strings parsing, features different pathways, and re-entrance as well. 
+
+beautifulsoup4
+BeautifulSoup can easily parse data out of HTML and XML. However, to do so, it needs a package and an exterior parser.
 '''

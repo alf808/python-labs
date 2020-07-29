7ba0cf8fe9640b6a1094878ddb844e6c00c54232
alf808 <alf@akademe.net>
2020-07-29 13:20:27 -1000

    completed variable arguments labs

--- 16_variable_arguments/16_02_kwargs.py
+++ 16_variable_arguments/16_02_kwargs.py
@@ -2,3 +2,10 @@
 Write a script with a function that demonstrates the use of **kwargs.
 
 '''
+def cook_this(**kwargs):
+    for k, v in kwargs.items():
+        print(f"{k}: {v}")
+
+cook_this(recipe="roast veggies", appliance="oven",
+            temp=350, cook_time="2 hours",
+            ingredients=['broccoli', 'carrots', 'cabbage', 'tomatoes'])

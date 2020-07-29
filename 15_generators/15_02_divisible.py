59dde8d09891e8bca9af7076edf7a248efb59e48
alf808 <alf@akademe.net>
2020-07-29 08:57:45 -1000

    finished divisible lab

--- 15_generators/15_02_divisible.py
+++ 15_generators/15_02_divisible.py
@@ -3,3 +3,6 @@ Create a Generator that loops over the given list and prints out only
 the items that are divisible by 1111.
 
 '''
+gen_ = (i for i in range(70000) if i % 1111 == 0)
+for i in gen_:
+    print(i)

0ca0c3113300e51431d65bf8743f956a7cec4ad6
alf808 <alf@akademe.net>
2020-07-29 09:34:03 -1000

    started enumerate labs

--- 13_aggregate_functions/13_01_enumerate.py
+++ 13_aggregate_functions/13_01_enumerate.py
@@ -1,3 +1,7 @@
 '''
 Demonstrate the use of the .enumerate() function.
 '''
+hawaii_islands = ["O`ahu", "Kaua`i", "Maui", "Hawai`i", "Moloka`i", "Lāna`i", "Ni`ihau", "Kaho`olawe"]
+
+for idx, island in enumerate(hawaii_islands, start=1):
+    print(f"{idx}: {island}")

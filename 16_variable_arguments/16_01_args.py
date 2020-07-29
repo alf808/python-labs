cf572a58e554890c461f1d31723bf1e424cd31c8
alf808 <alf@akademe.net>
2020-07-29 13:14:33 -1000

    started variable arguments labs

--- 16_variable_arguments/16_01_args.py
+++ 16_variable_arguments/16_01_args.py
@@ -2,3 +2,16 @@
 Write a script with a function that demonstrates the use of *args.
 
 '''
+def print_this(*args):
+    for input in args:
+        print(input)
+
+def sum_this(*args):
+    total = 0
+    for num in args:
+        total += num
+    return total
+
+print_this(35, 78, 21, 99999, 87)
+
+print(sum_this(35, 78, 21, 99999, 87))

6cb5caab9d99f7a8081dd030180dd098777dd855
alf808 <alf@akademe.net>
2020-07-29 22:15:29 -1000

    very first attempt to decorate

--- 17_decorators/17_01_wraps.py
+++ 17_decorators/17_01_wraps.py
@@ -2,3 +2,12 @@
 Write a decorator function that wraps text passed to it in a html <p> tag.
 
 '''
+def p_tag(func):
+    def wrapper_p_tag(text):
+        return f"<p>{func(text)}</p>"
+    return wrapper_p_tag
+    
+
+@p_tag
+def get_text(blurb):
+    return "wrapped in p\'s"

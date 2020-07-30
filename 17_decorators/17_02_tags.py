65ae7042c0dfa09dd8a37e10e0c93cfc0427c9e2
alf808 <alf@akademe.net>
2020-07-29 22:28:56 -1000

    completed decorator labs

--- 17_decorators/17_02_tags.py
+++ 17_decorators/17_02_tags.py
@@ -3,3 +3,14 @@ Improve the decorator from the previous exercise by allowing it to take
 any specified HTML tag as an input - making it more general.
 
 '''
+def tag_it(func):
+    def wrapper_tag_it(text, tag_name):
+        return f"<{tag_name}>{func(text, tag_name)}</{tag_name}>"
+    return wrapper_tag_it
+    
+
+@tag_it
+def get_text(blurb, tag):
+    return blurb
+
+print(get_text("now is the time", "h1"))
